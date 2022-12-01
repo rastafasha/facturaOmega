ecf059a699b008dfee6943b31c4a264e7131427e
Malcolm Cordova <mercadocreativo@gmail.com>
2022-12-01 08:53:38 -0400

    v2 con arreglo en facturas

--- src/app/pages/boletos/importar-boletos-v3/importar-boletos-v3.component.ts
+++ src/app/pages/boletos/importar-boletos-v3/importar-boletos-v3.component.ts
@@ -7,273 +7,237 @@
  * v2.0.0 - Se integra configuracion para importacion para IAV y TAG
  * v1.0.0 - Formulario de importacion dinamico, se permite importar varios archivos simultaneamente
  */
-import { Component, ViewChild, OnDestroy, OnInit } from '@angular/core';
-import { MatStepper } from '@angular/material/stepper';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+ import { Component, ViewChild, OnDestroy, OnInit } from '@angular/core';
+ import { MatStepper } from '@angular/material/stepper';
+ import { Router } from '@angular/router';
+ import { Subscription } from 'rxjs';
+ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-// Servicios
-import { MessagesService } from '../../../services/messages.service';
-import { BoletosService } from '../../../services/boletos.service';
-import ISesion from '../../../classes/interface.sesion';
-import { UsuariosService } from '../../../services/usuarios.service';
-import { TarifasService } from '../../../services/tarifas.service';
+ // Servicios
+ import { MessagesService } from '../../../services/messages.service';
+ import { BoletosService } from '../../../services/boletos.service';
+ import ISesion from '../../../classes/interface.sesion';
+ import { UsuariosService } from '../../../services/usuarios.service';
+ import { TarifasService } from '../../../services/tarifas.service';
 
-// Clases
-import { IImportacion } from '../../../classes/interface.importacion';
-import { TicketUtils } from '../../../classes/ticket';
-import { TIPOS_IMPORTACION } from '../../../config/config';
-import { Tarifa } from '../../../classes/tarifa';
-import { TipoImport, Conf } from 'src/app/models/tipoImportacion';
-import { Boleto } from 'src/app/classes/class.boleto';
+ // Clases
+ import { IImportacion } from '../../../classes/interface.importacion';
+ import { TicketUtils } from '../../../classes/ticket';
+ import { TIPOS_IMPORTACION } from '../../../config/config';
+ import { Tarifa } from '../../../classes/tarifa';
 
-@Component({
-  selector: 'app-importar-boletos-v3',
-  templateUrl: './importar-boletos-v3.component.html',
-  styles: [
-  ]
-})
-/**
- * @class ImportarBoletosV3Component
- * @description Clase controladora de la importacion de boletos
- */
-export class ImportarBoletosV3Component implements OnDestroy, OnInit {
+ @Component({
+   selector: 'app-importar-boletos-v3',
+   templateUrl: './importar-boletos-v3.component.html',
+   styles: [
+   ]
+ })
+ /**
+  * @class ImportarBoletosV3Component
+  * @description Clase controladora de la importacion de boletos
+  */
+ export class ImportarBoletosV3Component implements OnDestroy, OnInit {
 
-  importados: IImportacion[] = [];
-  loading: boolean;
-  form: FormGroup;
-  sesion: ISesion;
-  tarifas: Tarifa[] = [];
-  importConfigSelected: any = TIPOS_IMPORTACION;
-  tipo:any = TIPOS_IMPORTACION;
+   importados: IImportacion[] = [];
+   loading: boolean;
+   form: FormGroup;
+   sesion: ISesion;
+   tarifas: Tarifa[] = [];
+   importConfigSelected: any;
 
-  tipoimp;
+   columns: string[] = ['carril', 'fecha', 'folio', 'hora', 'pago', 'senal', 'tarifa'];
+   dataPreview: any[] = [];
 
-  boletos: Boleto;
+   @ViewChild(MatStepper) stepper: MatStepper;  // Setepper
 
-  tipoF:any = TipoImport;
-  conf:any = Conf;
+   suscripcion: Subscription = new Subscription();
 
-  columns: string[] = ['carril', 'fecha', 'folio', 'hora', 'pago', 'senal', 'tarifa'];
-  dataPreview: any[] = [];
+   readonly COLS: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+   readonly TIPOS_IMPORTACION: any[] = TIPOS_IMPORTACION;
 
-  @ViewChild(MatStepper) stepper: MatStepper;  // Setepper
-
-  suscripcion: Subscription = new Subscription();
-
-  readonly COLS: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  readonly TIPOS_IMPORTACION: any[] = TIPOS_IMPORTACION;
-
-  /**
-   * @constructor
-   * @param {BoletosService} _boletosService Servicio de operaciones con boletos
-   * @param {MessagesService} _messagesService Servicio de operaciones de mensajes UI
-   * @param {UsuariosService} _usuariosService Servicio de operaciones con usuarios del sistema
-   * @param {TarifasService} _tarifasService Servicio para consultar el catalofo de tarifas
-   * @param {Router} router Enrutamiento de la pagina
-   * @param {FormBuilder} fb Operaciones con formulario
-   */
-  constructor(private _boletosService: BoletosService,
-              private _messagesService: MessagesService,
-              private _usuariosService: UsuariosService,
-              private _tarifasService: TarifasService,
-              private router: Router,
-              private fb: FormBuilder) {
-    this.sesion = this._usuariosService.obtenerSesion();
-    this.tipoimp = this._boletosService.tipoI;
-    this.tipoimp = this._boletosService.tipoI;
-  } // constructor
+   /**
+    * @constructor
+    * @param {BoletosService} _boletosService Servicio de operaciones con boletos
+    * @param {MessagesService} _messagesService Servicio de operaciones de mensajes UI
+    * @param {UsuariosService} _usuariosService Servicio de operaciones con usuarios del sistema
+    * @param {TarifasService} _tarifasService Servicio para consultar el catalofo de tarifas
+    * @param {Router} router Enrutamiento de la pagina
+    * @param {FormBuilder} fb Operaciones con formulario
+    */
+   constructor(private _boletosService: BoletosService,
+               private _messagesService: MessagesService,
+               private _usuariosService: UsuariosService,
+               private _tarifasService: TarifasService,
+               private router: Router,
+               private fb: FormBuilder) {
+     this.sesion = this._usuariosService.obtenerSesion();
+   } // constructor
 
 
-  /**
-   * @method ngOnInit
-   * @description Inicializacion del componente
-   */
-  ngOnInit(): void {
-    this.form = this.fb.group({
-      enabled: false,
-      tipo_importacion: [null, Validators.required],
-      // tipo_importacion: [TIPOS_IMPORTACION[0], Validators.required],
-      filaInicial  : [null, [Validators.required, Validators.min(1)]],
-      fecha        : [null,  Validators.required],
-      hora         : [null,  Validators.required],
-      carril       : [null,  Validators.required],
-      senal        : [null,  Validators.required],
-      tarifa       : [null,  Validators.required],
-      pago         : [null, Validators.required],
-      folio        : [null, Validators.required]
-    });
+   /**
+    * @method ngOnInit
+    * @description Inicializacion del componente
+    */
+   ngOnInit(): void {
+     this.form = this.fb.group({
+       enabled: false,
+       tipo_importacion: [TIPOS_IMPORTACION[0], Validators.required],
+       filaInicial  : [null, [Validators.required, Validators.min(1)]],
+       fecha        : [null,  Validators.required],
+       hora         : [null,  Validators.required],
+       carril       : [null,  Validators.required],
+       senal        : [null,  Validators.required],
+       tarifa       : [null,  Validators.required],
+       pago         : [null, Validators.required],
+       folio        : [null, Validators.required]
+     });
 
-    this.seleccionarConfiguracion(this.tipoF, this.conf);
-    this.form.disable();
-    this.loadTiposList();
-    // console.log(this.boletos)
-  } // ngOnInit
+     this.seleccionarConfiguracion(TIPOS_IMPORTACION[0]);
+     this.form.disable();
+   } // ngOnInit
 
-  /**
-   * @method ngOnDestroy
-   * @description Destruccion del componente
-   */
-  ngOnDestroy(): void {
-    this.suscripcion.unsubscribe();
-
-  } // ngOnDestroy
-
-  //carga usos desde la app
-  loadTiposList(): void{
-    this.tipoF = this._boletosService.getTipos();
-    console.log(this.tipoF);
-
-    if(this.tipoF.id == '1'){
-      return this.tipoF[0].desc;
-    }if(this.tipoF.id == '2'){
-      return this.tipoF[1].desc;
-    }
+   /**
+    * @method ngOnDestroy
+    * @description Destruccion del componente
+    */
+   ngOnDestroy(): void {
+     this.suscripcion.unsubscribe();
+   } // ngOnDestroy
 
 
-  }
+   /**
+    * @method paso1
+    * @description Configuracion de la importacion
+    */
+   paso1(): void {
+     this.stepper.next();
+   }
 
 
-  /**
-   * @method paso1
-   * @description Configuracion de la importacion
-   */
-  paso1(): void {
-    this.stepper.next();
-  }
+   /**
+    * @method paso2
+    * @param evt Evento recibido del input type file
+    * @description Seleccion de los archivos a importar
+    */
+   async paso2(evt: any) {
+     // Inicializamos valores
+     const { id }: any = this.form.get('tipo_importacion').value;
+     this.importados = [];
+     this.loading = true;
+
+     // Recorremos ls archivos seleccionados
+     const target: DataTransfer = (evt.target) as DataTransfer;
+
+     for (let i = 0; i < target.files.length; i++) {
+       const config: any = {
+         filaInicial: this.form.get('filaInicial').value,
+         fecha: this.form.get('fecha').value,
+         hora: this.form.get('hora').value,
+         carril: this.form.get('carril').value,
+         senal: this.form.get('senal').value,
+         tarifa: this.form.get('tarifa').value,
+         pago: this.form.get('pago').value,
+         folio: this.form.get('folio').value
+       };
+
+       if (id === 2) {
+         // IAVE y TAG
+         await TicketUtils.importartXLS(target.files.item(i), config, this.tarifas)
+           .then(imp => this.importados.push(imp))
+           .catch(err => this._messagesService.error(err));
+       } else {
+         // NOR y CRE
+         await TicketUtils.importartXLS(target.files.item(i), config)
+           .then(imp => this.importados.push(imp))
+           .catch(err => this._messagesService.error(err));
+       }
+     } // for i (recorrido de archivos)
+
+     this.loading = false;
+     this.stepper.next();
+   } // paso1
 
 
-  /**
-   * @method paso2
-   * @param evt Evento recibido del input type file
-   * @description Seleccion de los archivos a importar
-   */
-  async paso2(evt: any) {
-    // Inicializamos valores
-    const { id }: any = this.form.get('tipo_importacion').value;
-    this.importados = [];
-    this.loading = true;
+   /**
+    * @method paso3
+    * @description Importacion de los archivos
+    */
+   async paso3() {
+     this.loading = true;
+     let error = false;
+
+     for (let i = 0; i < this.importados.length; i++) {
+       this.importados[i].message = 'Importando...';
+       await this.importar(this.importados[i].data)
+         .then(() => this._messagesService.toast(`${this.importados[i].file.name} importado correctamente`, 'OK'))
+         .catch(err => {
+           error = true;
+           this.importados[i].err = this._messagesService.error(err, this.importados[i].file.name);
+           this.importados[i].message = this.importados[i].err.messages;
+         });
+       this.importados[i].message = 'Importado';
+     }
+
+     if (error) {
+       this.loading = false;
+     } else {
+       this.router.navigate([`/${this.sesion.path}/boletos`]);
+     }
+   } // paso2
 
 
-
-    // Recorremos ls archivos seleccionados
-    const target: DataTransfer = (evt.target) as DataTransfer;
-
-    for (let i = 0; i < target.files.length; i++) {
-      const config: any = {
-        filaInicial: this.form.get('filaInicial').value,
-        fecha: this.form.get('fecha').value,
-        hora: this.form.get('hora').value,
-        carril: this.form.get('carril').value,
-        senal: this.form.get('senal').value,
-        tarifa: this.form.get('tarifa').value,
-        pago: this.form.get('pago').value,
-        folio: this.form.get('folio').value
-      };
+   /**
+    * @method importar
+    * @description Importacion de la data
+    * @param data Data a importar
+    * @returns {Promise}
+    */
+   importar(data: any): Promise<void> {
+     return new Promise((resolve, reject) => {
+       this.suscripcion = this._boletosService.importar(data)
+         .subscribe(() => resolve(), err => reject(err));
+     });
+   } // paso2
 
 
+   /**
+    * @method seleccionarConfiguracion
+    * @description Selecciona el tipo de configuracion de importacion
+    * @param tipoImportacion Tipo de importacion a establecer
+    */
+   seleccionarConfiguracion(tipoImportacion: any): void {
+     const { config } = tipoImportacion;
+     this.importConfigSelected = tipoImportacion;
+     this.form.get('filaInicial').setValue(config.fila_inicial);
+     this.form.get('fecha').setValue(config.fecha);
+     this.form.get('hora').setValue(config.hora);
+     this.form.get('carril').setValue(config.carril);
+     this.form.get('senal').setValue(config.senal);
+     this.form.get('pago').setValue(config.pago);
+     this.form.get('folio').setValue(config.folio);
 
-      if (id === 2) {
-        // IAVE y TAG
-        await TicketUtils.importartXLS(target.files.item(i), config, this.tarifas)
-          .then(imp => this.importados.push(imp))
-          .catch(err => this._messagesService.error(err));
-      } else {
-        // NOR y CRE
-        await TicketUtils.importartXLS(target.files.item(i), config)
-          .then(imp => this.importados.push(imp))
-          .catch(err => this._messagesService.error(err));
-      }
-    } // for i (recorrido de archivos)
-
-    this.loading = false;
-    this.stepper.next();
-    console.log(this.importados);
-  } // paso1
-
-
-  /**
-   * @method paso3
-   * @description Importacion de los archivos
-   */
-  async paso3() {debugger
-    this.loading = true;
-    let error = false;
-
-    for (let i = 0; i < this.importados.length; i++) {
-      this.importados[i].message = 'Importando...';
-      await this.importar(this.importados[i].data)
-        .then(() => this._messagesService.toast(`${this.importados[i].file.name} importado correctamente`, 'OK'))
-        .catch(err => {
-          error = true;
-          this.importados[i].err = this._messagesService.error(err, this.importados[i].file.name);
-          this.importados[i].message = this.importados[i].err.messages;
-        });
-      this.importados[i].message = 'Importado';
-    }
-
-    if (error) {
-      this.loading = false;
-    } else {
-      this.router.navigate([`/${this.sesion.path}/boletos`]);
-    }
-  } // paso2
+     if (tipoImportacion.id === 2) {
+       this.form.get('tarifa').setValue(null);
+       this.form.get('tarifa').disable();
+       // Obtenemos catalogo de tarifas
+       this.loading = true;
+       this.suscripcion = this._tarifasService.cargarTodos()
+       .subscribe(({ tarifas }) => {
+         this.tarifas = tarifas;
+         this.loading = false;
+       }, err => {
+         this.loading = false;
+         this._messagesService.error(err);
+       });
+     } else {
+       this.form.get('tarifa').setValue(config.tarifa);
+       this.form.get('tarifa').enable();
+     }
+   }
 
 
-  /**
-   * @method importar
-   * @description Importacion de la data
-   * @param data Data a importar
-   * @returns {Promise}
-   */
-  importar(data: any): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.suscripcion = this._boletosService.importar(data)
-        .subscribe(() => resolve(), err => reject(err));
-    });
-  } // paso2
+   previsualizar(data: any[]) {
 
+   }
 
-  /**
-   * @method seleccionarConfiguracion
-   * @description Selecciona el tipo de configuracion de importacion
-   * @param tipoImportacion Tipo de importacion a establecer
-   */
-  seleccionarConfiguracion(tipoF: TipoImport, conf: Conf): void {
-    const { config } = tipoF;
-
-    this.importConfigSelected = tipoF;
-    this.form.get('filaInicial').setValue(conf.fila_inicial);
-    this.form.get('fecha').setValue(conf.fecha);
-    this.form.get('hora').setValue(conf.hora);
-    this.form.get('carril').setValue(conf.carril);
-    this.form.get('senal').setValue(conf.senal);
-    this.form.get('pago').setValue(conf.pago);
-    this.form.get('folio').setValue(conf.folio);
-
-    if (tipoF.id === 2) {
-      this.form.get('tarifa').setValue(null);
-      this.form.get('tarifa').disable();
-      // Obtenemos catalogo de tarifas
-      this.loading = true;
-      this.suscripcion = this._tarifasService.cargarTodos()
-      .subscribe(({ tarifas }) => {
-        this.tarifas = tarifas;
-        this.loading = false;
-      }, err => {
-        this.loading = false;
-        this._messagesService.error(err);
-      });
-    } else {
-      this.form.get('tarifa').setValue(conf.tarifa);
-      this.form.get('tarifa').enable();
-    }
-  }
-
-
-  previsualizar(data: any[]) {
-
-  }
-
-}
+ }
